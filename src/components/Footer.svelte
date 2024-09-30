d9f7c7e752c30a9a05a821541d6c66a5e1bf6760
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2024-09-30 18:42:12 -0500

    Initial Commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,37 @@
+<footer
+    class="py-20 sm:py-32 bg-black border-t border-solid border-orange-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+>
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Contact Me
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center rounded-lg">
+        <!-- <div
+            class="flex items-center gap-4 sm:gap-6 text-2xl sm:text-3xl md:text-4xl mx-auto"
+        >
+            <a>
+                <i class="fa-brands fa-linkedin-in" />
+            </a>
+            <a>
+                <i class="fa-brands fa-github" />
+            </a>
+        </div>
+        <p class="mx-auto text-sm opacity-70">or</p>
+        <p class="">Hugh_jass@gmail.com</p> -->
+        <p><b class="pr-2">Email</b>abiepajaro@gmail.com</p>
+        <p>
+        </p>
+        <p>
+            <b class="pr-2">Contact Number:</b>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a target="_blank" class="text-orange-400"
+                >09299716043<sup class=""
+                    ><span class="text-xs scale-75 pl-0.5"
+                        ><i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        /></span
+                    ></sup
+                ></a
+            >
+        </p>
+    </div>
+</footer>
