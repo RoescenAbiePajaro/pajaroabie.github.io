e229a9d27e05a9bdf814e5796b7d42f7bda1dec4
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2024-09-30 19:17:16 -0500

    Initial commit

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,7 +2,7 @@
     class="py-20 sm:py-32 bg-black border-t border-solid border-orange-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
 >
     <p class="px-4 py-2 bg-white text-slate-950 font-medium">
-        Contact Me
+        Contact Me.
     </p>
     <div class="flex flex-col gap-4 items-center justify-center rounded-lg">
          <div
